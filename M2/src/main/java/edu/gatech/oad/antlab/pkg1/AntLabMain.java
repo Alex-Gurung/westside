5bc9d2e1c9a59793a4cfd3bba9f7a1a8b2035e53
Aman Jain <aman.jain@gatech.edu>
2019-01-23 15:39:38 -0500

    removed all merge comments

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -76,10 +76,6 @@ import edu.gatech.oad.antlab.pkg2.*;
                   Person5 p5 = new Person5("Jon");
                   toPrint += p5.toString("jwomack30");
 
-<<<<<<< HEAD
->>>>>>> 5f4b2a9f01d3ff101c3aa91c89226ca25bae9d3c
-=======
->>>>>>> 1ec71be9c4a00fb8f4c44323d2f5a21d5f7db2f7
         System.out.println(toPrint);
 
 
@@ -94,21 +90,4 @@ import edu.gatech.oad.antlab.pkg2.*;
         new AntLabMain().printOutMessage();
 
      }
-
-
-
-
-<<<<<<< HEAD
  }
-=======
-
-     }
-
-
-
-
- }
->>>>>>> 5f4b2a9f01d3ff101c3aa91c89226ca25bae9d3c
-=======
- }
->>>>>>> 1ec71be9c4a00fb8f4c44323d2f5a21d5f7db2f7
