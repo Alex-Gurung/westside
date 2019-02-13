752add6913d8d633b8e2ff89435fed7f94ce35f0
Alex Gurung <aag1234@gmail.com>
2019-02-13 15:59:05 -0500

    added test Toast

--- SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.spacetrader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "This is a test", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
     }
 }
