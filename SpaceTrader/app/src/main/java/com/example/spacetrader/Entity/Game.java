e60e97679b9de162b9de2b1de80a93354e5363df
Alex Gurung <aag1234@gmail.com>
2019-02-15 15:38:22 -0500

    Added Game

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Game.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Game.java
@@ -1,4 +1,15 @@
 package com.example.spacetrader.Entity;
 
 public class Game {
+    GameDifficulty gameDifficulty;
+    Player player;
+
+    public Game(Player player) {
+        this(GameDifficulty.BEGINNER, player);
+    }
+    public Game(GameDifficulty gameDifficulty, Player player) {
+        this.gameDifficulty = gameDifficulty;
+        this.player = player;
+    }
+
 }
