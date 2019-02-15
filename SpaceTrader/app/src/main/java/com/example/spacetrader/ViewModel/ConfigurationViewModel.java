aba4fb978af35570e7c80f4d72b91a1b8d347825
Aman Jain <aman.jain@gatech.edu>
2019-02-15 16:29:54 -0500

    made initializeGame static

--- SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
@@ -3,6 +3,7 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
+import com.example.spacetrader.Entity.Game;
 import com.example.spacetrader.Entity.GameDifficulty;
 import com.example.spacetrader.Entity.Player;
 import com.example.spacetrader.View.ConfigurationActivity;
@@ -13,7 +14,7 @@ public class ConfigurationViewModel extends AndroidViewModel {
         super(application);
     }
 
-    public void initializeGame(String name, int fighter, int engineer, int pilot, int trader, GameDifficulty gd) {
+    public static void initializeGame(String name, int fighter, int engineer, int pilot, int trader, GameDifficulty gd) {
         Player player = new Player(pilot, fighter, engineer, trader, name);
         Game game = new Game(gd, player);
     }
