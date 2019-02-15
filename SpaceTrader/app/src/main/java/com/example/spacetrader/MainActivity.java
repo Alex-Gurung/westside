6fecb78ca277fbb892779c6598e4ff03404c9c8e
Alex Gurung <aag1234@gmail.com>
2019-02-15 15:44:50 -0500

    added bracket

--- SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.spacetrader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "This is a test", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_configuration);
+
+
     }
 }
