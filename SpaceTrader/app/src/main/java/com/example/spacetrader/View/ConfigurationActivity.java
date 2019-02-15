b92d0a9934032699cb383553332baf29a51ea984
Jaden <jade234876@gmail.com>
2019-02-15 16:02:59 -0500

    extended ConfActivity by AppCompactActivity

--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
@@ -1,6 +1,7 @@
 package com.example.spacetrader.View;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -8,7 +9,7 @@ import android.widget.Spinner;
 
 import com.example.spacetrader.R;
 
-public class ConfigurationActivity {
+public class ConfigurationActivity extends AppCompatActivity {
 
     private EditText nameField;
     private Spinner difficultySpinner;
@@ -29,7 +30,7 @@ public class ConfigurationActivity {
          * Grab the dialog widgets so we can get info for later
          */
         nameField = findViewById(R.id.character_name_input);
-        difficultySpinner = findByViewId(R.id.gameDifficulty_spinner);
+        difficultySpinner = findViewById(R.id.gameDifficulty_spinner);
 
         /*
           Set up the adapter to display the allowable majors in the spinner
