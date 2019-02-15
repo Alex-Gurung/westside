61cf17adc76b073036c3553eadc1bf440386265f
jonathanglennwomack <jonathanglennwomack@gmail.com>
2019-02-15 15:41:43 -0500

    check skill points

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
@@ -21,6 +21,8 @@ public class Player {
         this.name = name;
     }
 
-
+    public int getTotalSkillPoints() {
+        return pilotSkillPoints + fighterSkillPoints + engineerSkillPoints + traderSkillPoints;
+    }
 
 }
--- SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
@@ -3,9 +3,14 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
+import com.example.spacetrader.Entity.Player;
+
 public class ConfigurationViewModel extends AndroidViewModel {
 
     public ConfigurationViewModel (@NonNull Application application)
         super(application);
+    }
+    public boolean skillPointChecker(Player player) {
+        return (player.getTotalSkillPoints() == 16) ? true: false;
     }
 }
