fa070030b1d8dc4b4fe3459f662767a555fa47f0
Aman Jain <aman.jain@gatech.edu>
2019-02-15 16:29:03 -0500

    Changed Config View Model

--- SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
@@ -3,6 +3,7 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
+import com.example.spacetrader.Entity.GameDifficulty;
 import com.example.spacetrader.Entity.Player;
 import com.example.spacetrader.View.ConfigurationActivity;
 
@@ -11,7 +12,9 @@ public class ConfigurationViewModel extends AndroidViewModel {
     public ConfigurationViewModel (@NonNull Application application) {
         super(application);
     }
-    public boolean skillPointChecker(ConfigurationActivity activity) {
-        return (activity.getTotalSkillPoints() == 16) ? true: false;
+
+    public void initializeGame(String name, int fighter, int engineer, int pilot, int trader, GameDifficulty gd) {
+        Player player = new Player(pilot, fighter, engineer, trader, name);
+        Game game = new Game(gd, player);
     }
 }
