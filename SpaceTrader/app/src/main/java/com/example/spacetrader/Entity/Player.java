9bad66e3999194889cebca3eb0f0a553b0fa245f
Aman Jain <aman.jain@gatech.edu>
2019-02-15 15:31:36 -0500

    added a name private field to Player

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
@@ -7,15 +7,18 @@ public class Player {
     private int engineerSkillPoints;
     private int traderSkillPoints;
 
+    private String name;
+
     private Ship ship = new Ship(ShipType.GNAT);
 
     private int credits = 1000;
 
-    public Player(int pilotSkillPoints, int fighterSkillPoints, int engineerSkillPoints, int traderSkillPoints) {
+    public Player(int pilotSkillPoints, int fighterSkillPoints, int engineerSkillPoints, int traderSkillPoints, String name) {
         this.engineerSkillPoints = engineerSkillPoints;
         this.fighterSkillPoints = fighterSkillPoints;
         this.pilotSkillPoints = pilotSkillPoints;
         this.traderSkillPoints = traderSkillPoints;
+        this.name = name;
     }
 
 
