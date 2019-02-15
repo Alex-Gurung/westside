3e71952e903043c3e57a9b7847a24e8290893497
Aman Jain <aman.jain@gatech.edu>
2019-02-15 15:52:47 -0500

    working on main activity

--- SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.spacetrader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "This is a test", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_configuration);
+
+        startButton = findViewById(R.id.start_button);
     }
 }
