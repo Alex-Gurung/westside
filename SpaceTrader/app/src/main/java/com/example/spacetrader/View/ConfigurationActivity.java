b2003906bfa9daf5c9d6ccd7bfb59a400720205f
Aman Jain <aman.jain@gatech.edu>
2019-02-15 15:49:03 -0500

    Edited activity_main to be the default opening screen

--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
@@ -1,4 +1,38 @@
 package com.example.spacetrader.View;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
 public class ConfigurationActivity {
+
+    private EditText nameField;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+
+        /*
+         * Disables actionbar back button
+         */
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setHomeButtonEnabled(false);
+        }
+
+        /*
+         * Grab the dialog widgets so we can get info for later
+         */
+        nameField = findViewById(R.id.character_name_input);
+
+        /*
+          Set up the adapter to display the allowable majors in the spinner
+         */
+
+    }
+
+    public void onBeginPressed(View view) {
+
+    }
 }
