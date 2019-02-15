85f08c0acc509d0d681e7115c1a41de7e51ae2dd
Aman Jain <aman.jain@gatech.edu>
2019-02-15 15:28:09 -0500

    Update Player.java

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
@@ -1,4 +1,23 @@
 package com.example.spacetrader.Entity;
 
 public class Player {
+
+    private int pilotSkillPoints;
+    private int fighterSkillPoints;
+    private int engineerSkillPoints;
+    private int traderSkillPoints;
+
+    private Ship ship = new Ship(ShipType.GNAT);
+
+    private int credits = 1000;
+
+    public Player(int pilotSkillPoints, int fighterSkillPoints, int engineerSkillPoints, int traderSkillPoints) {
+        this.engineerSkillPoints = engineerSkillPoints;
+        this.fighterSkillPoints = fighterSkillPoints;
+        this.pilotSkillPoints = pilotSkillPoints;
+        this.traderSkillPoints = traderSkillPoints;
+    }
+
+
+
 }
