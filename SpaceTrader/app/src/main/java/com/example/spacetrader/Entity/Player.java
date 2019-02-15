3012f23f3f1681743da82a00136ad377a3508f1a
Aman Jain <aman.jain@gatech.edu>
2019-02-15 16:08:07 -0500

    Edited Entity files

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Player.java
@@ -21,8 +21,19 @@ public class Player {
         this.name = name;
     }
 
-    public int getTotalSkillPoints() {
-        return pilotSkillPoints + fighterSkillPoints + engineerSkillPoints + traderSkillPoints;
+    @Override
+    public String toString() {
+        String s = "";
+        s += "Hello, my name is " + name + "\n";
+        s += "I own a ship of model " + ship.toString() + "\n";
+        s += "I have " + fighterSkillPoints + " fighter skill points" + "\n";
+        s += "I have " + traderSkillPoints + " trader skill points" + "\n";
+        s += "I have " + engineerSkillPoints + " engineer skill points" + "\n";
+        s += "I have " + pilotSkillPoints + " pilot skill points" + "\n";
+        s += "I have " + credits + " credits" + "\n";
+        return s;
     }
+
+
 
 }
--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/ShipType.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/ShipType.java
@@ -1,5 +1,19 @@
 package com.example.spacetrader.Entity;
 
 public enum ShipType {
-    GNAT;
+    //name, weapon slot, shield, gadget, escape pod, cargo bay, distance
+    GNAT("Gnat");
+    public String name;
+
+    ShipType(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+
+
+
 }
