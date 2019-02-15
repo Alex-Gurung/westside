ec3dec11f34de53d801fdff581416853322dc0a8
Alex Gurung <aag1234@gmail.com>
2019-02-15 15:51:41 -0500

    modified configuration activity, working on switching from main to config

--- SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.spacetrader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.spacetrader.View.ConfigurationActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "This is a test", Toast.LENGTH_SHORT).show();
-        setContentView(R.layout.activity_configuration);
+        setContentView(R.layout.activity_main);
 
+        Button goToConfig = (Button) findViewById(R.id.goToConfigButton);
+
+        goToConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, ConfigurationActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/ConfigurationActivity.java
@@ -1,4 +1,16 @@
 package com.example.spacetrader.View;
 
-public class ConfigurationActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.spacetrader.R;
+
+public class ConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+
+    }
 }
