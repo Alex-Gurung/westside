6bacdb9fcfef5bf41105b219ac3f175c020915b8
Aman Jain <aman.jain@gatech.edu>
2019-02-21 20:13:51 -0500

    Finished Basic Tech Level

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/TechLevel.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/TechLevel.java
@@ -1,5 +1,18 @@
 package com.example.spacetrader.Entity;
 
 public enum TechLevel {
+    PREAGRICULTURAL("Pre-Agricultural"),
+    AGRICULTURAL("Agricultural"),
+    MEDIEVAL("Medieval"),
+    RENAISSANCE("Renaissance"),
+    EARLYINDUSTRIAL("Early-Industrial"),
+    INDUSTRIAL("Industrial"),
+    POSTINDUSTRIAL("Post-Industrial"),
+    HITECH("Hi-Tech");
 
+    private String name;
+
+    TechLevel(String name) {
+        this.name = name;
+    }
 }
