440e1803a84b6d14beb70f098f32e4e6fa63214e
Aman Jain <aman.jain@gatech.edu>
2019-02-21 20:20:04 -0500

    Finished a basic Politcal System enum

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/PoliticalSystem.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/PoliticalSystem.java
@@ -1,5 +1,27 @@
 package com.example.spacetrader.Entity;
 
 public enum PoliticalSystem {
+    ANARCHY("Anarchy"),
+    CAPITALISTSTATE("Capitalist State"),
+    COMMUNISTSTATE("Communist State"),
+    CONFEDERACY("COnfederacy"),
+    CORPORATESTATE("Corporate State"),
+    CYBERNETICSTATE("Cybernetic State"),
+    DEMOCRACY("Democracy"),
+    DICTATORSHIP("Dictatorship"),
+    FACISTSTATE("Facist State"),
+    FEUDALSTATE("Feudal Stae"),
+    MILITARYSTATE("Military State"),
+    MONARCHY("Monarchy"),
+    PACIFISTSTATE("Pacifist State"),
+    SOCIALISTSTATE("Socialist State"),
+    STATEOFSATORI("State of Satori"),
+    TECHNOCRACY("Technocracy"),
+    THEOCRACY("Theocracy");
 
+    private String name;
+
+    PoliticalSystem(String name) {
+        this.name = name;
+    }
 }
