f170c83edadaa8d807f058544f93f3b13cc51e9f
Jaden <jade234876@gmail.com>
2019-02-21 20:52:18 -0500

    java doc'ed all the methods/constructors we have

---
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Resources.java
@@ -0,0 +1,18 @@
+package com.example.spacetrader.Entity;
+
+public enum Resources {
+
+    NOSPECIALSOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WIERDMUSHROOMS,
+    LOSTSOFHERBS,
+    ARTISTIC,
+    WARLIKE
+}
