168f65e9c9e78d5e95e7d81560bdc74a8c18eb6a
Aman Jain <aman.jain@gatech.edu>
2019-02-21 20:26:24 -0500

    Made Location Class

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Location.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Location.java
@@ -1,4 +1,24 @@
 package com.example.spacetrader.Entity;
 
 public class Location {
+    private int x;
+    private int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return (x * 100 + y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof Location)) return false;
+        Location o = (Location) obj;
+        return (this.x == o.x && this.y == o.y);
+    }
 }
