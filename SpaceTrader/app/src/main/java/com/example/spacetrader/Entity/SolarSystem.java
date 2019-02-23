a236e44378d46eded222fa79d093f2b8c5a32aff
jonathanglennwomack <jonathanglennwomack@gmail.com>
2019-02-23 16:12:02 -0500

    Merge branch 'master' of /Users/jonwomack/Documents/CS 2340/westside with conflicts.

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/SolarSystem.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/SolarSystem.java
@@ -1,16 +1,12 @@
 package com.example.spacetrader.Entity;
 
-/**
- * A Solar System class that contains information for the Solar System's name, location, political
- * system, tech level, and whichever plante(s) that are contained in the Solar System.
-  */
 public class SolarSystem {
 
     private String name;
     private Location location;
-    PoliticalSystem politicalSystem;
-    TechLevel techLevel;
-    Planet planet;
+    private PoliticalSystem politicalSystem;
+    private TechLevel techLevel;
+    private Planet planet;
 
     /**
      * public constructor to initialize a SolarSystem object that has a random location, tech level,
@@ -32,18 +28,12 @@ public class SolarSystem {
     /**
      * constructor that takes in a randomized location
      *
-     * @param l of type Location that is the SolarSystem's random location.
+     * @param l of type Location that is the SolarSystems random location.
      */
     public SolarSystem(Location l) {
         this(l, PoliticalSystem.getRandomPoliticalSystem(), TechLevel.getRandomTechLevel(), "");
     }
 
-    /**
-     * constructor that takes in a location and name parameter
-     *
-     * @param l of type Location that is the Solar System's random location
-     * @param name of type String that is the Solar System's name
-     */
     public SolarSystem(Location l, String name) {
         this(l, PoliticalSystem.getRandomPoliticalSystem(), TechLevel.getRandomTechLevel(), name);
     }
