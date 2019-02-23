87bef67f374e7f7e811202c9747ca8e63a49af4b
Alex Gurung <aag1234@gmail.com>
2019-02-23 15:11:23 -0500

    Added interactions with viewmodel for game creation

--- SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/ViewModel/ConfigurationViewModel.java
@@ -6,16 +6,21 @@ import android.support.annotation.NonNull;
 import com.example.spacetrader.Entity.Game;
 import com.example.spacetrader.Entity.Player;
 import com.example.spacetrader.Entity.GameDifficulty;
+import com.example.spacetrader.Model.GameInteractor;
+import com.example.spacetrader.Model.Model;
 
 public class ConfigurationViewModel extends AndroidViewModel {
+    private GameInteractor interactor;
 
     public ConfigurationViewModel (@NonNull Application application) {
         super(application);
+        interactor = Model.getInstance().getGameInteractor();
     }
 
-    public static void initializeGame(String name, int fighter, int engineer, int pilot, int trader, GameDifficulty gd) {
+    public void initializeGame(String name, int fighter, int engineer, int pilot, int trader, GameDifficulty gd) {
         Player player = new Player(pilot, fighter, engineer, trader, name);
         Game game = new Game(gd, player);
 
+        interactor.setGame(game);
     }
 }
