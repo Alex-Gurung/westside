c52fb1f840793b03574d35c2d444dbfc52c9d1a4
Theresa Hsieh <theresaying.hsieh@gmail.com>
2019-02-24 17:24:25 -0500

    Created the Character abstract class

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Character.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Character.java
@@ -1,4 +1,39 @@
 package com.example.spacetrader.Entity;
 
-public interface Character {
+public abstract class Character {
+
+    private String name;
+    private SolarSystem currentSolarSystem;
+    private Ship myShip;
+
+    public Character (String name, SolarSystem solarSystem, Ship ship) {
+        this.name = name;
+        currentSolarSystem = solarSystem;
+        myShip = ship;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SolarSystem getCurrentSolarSystem() {
+        return currentSolarSystem;
+    }
+
+    public void setCurrentSolarSystem(SolarSystem currentSolarSystem) {
+        this.currentSolarSystem = currentSolarSystem;
+    }
+
+    public Ship getMyShip() {
+        return myShip;
+    }
+
+    public void setMyShip(Ship myShip) {
+        this.myShip = myShip;
+    }
+
+
 }
