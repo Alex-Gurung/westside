bf39fcb8a98c7613c94c779e2a49497a64a1abc4
Aman Jain <aman.jain@gatech.edu>
2019-02-24 17:30:35 -0500

    Modified Good

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Good.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/Good.java
@@ -1,4 +1,40 @@
 package com.example.spacetrader.Entity;
 
 public class Good {
+    private GoodType goodtype;
+    private double price;
+
+    /**
+     * constructor
+     * @param goodtype
+     */
+    public Good (GoodType goodtype) {
+        this.goodtype = goodtype;
+        price = 0;
+    }
+
+    /**
+     * sets the price for the good
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * returns the price of this good
+     * @return
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * returns the type of good this is
+     * @return this Good's goodtype
+     */
+    public GoodType getGoodType() {
+        return goodtype;
+    }
+
 }
