2a8d92c365b6187c2071afcd7dba063eef5b5c33
Jaden <jade234876@gmail.com>
2019-02-27 15:46:46 -0500

    java doced cargoAdapter

--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/CargoAdapter.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/CargoAdapter.java
@@ -15,29 +15,45 @@ import com.example.spacetrader.R;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Adapter Class that allows a list of goods in the player's cargo to be displayed on a screen,
+ * as well as be modified (or not) based on the actions of the player
+ */
 public class CargoAdapter extends RecyclerView.Adapter<CargoAdapter.CargoViewHolder>{
-    /** a copy of the list of cargo in the model */
+    /** a copy of the list of Goods in the model */
     private List<Good> cargo = new ArrayList<>();
 
-    /** a listener for a touch event on the cargoItem */
+    /** a listener for a touch event on the cargo good */
     private OnCargoGoodClickListener listener;
 
+    /**
+     * View holder that displays all pof the goods in the player's cargo hold
+     *
+     * @param parent of type ViewGroup to help with displaying a good item in the cargo hold
+     * @param i of type int, but is unused for this context
+     * @return a Cargo View holder that shows a single good item in the entire cargo hold
+     */
     @NonNull
     @Override
     public CargoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
 
-        // hook up to the view for a single student in the system
+        // hook up to the view for a single good in the system
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.cargo_item, parent, false);
 
         return new CargoViewHolder(itemView);
     }
 
-
+    /**
+     * method to bind the view holder to certain attributes given to the view holder
+     *
+     * @param holder of type CargoViewHoldert whose text value will be set to the name of the good
+     * @param position of type int to get the position of the cargo in the LOGCAT
+     */
     @Override
     public void onBindViewHolder(@NonNull CargoViewHolder holder, int position) {
 
-        //bind the cargo data for one cargoItem
+        //bind the cargo item data for the cargo
         Good cargoItem = cargo.get(position);
 
         Log.d("APP", "Binding: " + position + " " + cargo.get(position));
@@ -49,11 +65,21 @@ public class CargoAdapter extends RecyclerView.Adapter<CargoAdapter.CargoViewHol
 
     }
 
+    /**
+     * method that returns the size of the player's cargo
+     *
+     * @return ant in representation of the player's cargo hold
+     */
     @Override
     public int getItemCount() {
         return cargo.size();
     }
 
+    /**
+     * set's the cargo of the player
+     *
+     * @param cargo a list of Goods contained in the cargo
+     */
     public void setCargoList(List<Good> cargo) {
         this.cargo = cargo;
         notifyDataSetChanged();
@@ -61,14 +87,18 @@ public class CargoAdapter extends RecyclerView.Adapter<CargoAdapter.CargoViewHol
 
 
     /**
-     * This is a holder for the widgets associated with a single entry in the list of cargo
+     * This is a holder for the widgets associated with a single entry in the list of goods
      */
     class CargoViewHolder extends RecyclerView.ViewHolder {
         private TextView name;
         private TextView price;
         private Button sellButton;
 
-
+        /**
+         * Method that creates the view layout of what each good will look like in the cargo hold
+         *
+         * @param itemView
+         */
         public CargoViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.cargo_name);
@@ -77,6 +107,11 @@ public class CargoAdapter extends RecyclerView.Adapter<CargoAdapter.CargoViewHol
 
             sellButton.setOnClickListener(new View.OnClickListener() {
 
+                /**
+                 * a method that reacts to the user pressing on the sell button on the screen
+                 *
+                 * @param view of type View that passes in the view the user clicked on
+                 */
                 @Override
                 public void onClick(View view) {
                     int position = getAdapterPosition();
@@ -86,14 +121,22 @@ public class CargoAdapter extends RecyclerView.Adapter<CargoAdapter.CargoViewHol
                     }
                 }
             });
-
         }
     }
 
+    /**
+     * interface with one method to be implemented in the onClick method
+     */
     public interface OnCargoGoodClickListener {
         void onCargoGoodSell(int position);
     }
 
+    /**
+     * sets a listener for each good item in the cargo hold
+     *
+     * @param listener of type OnCargoGoodClickListener that sets this instance's listener
+     *
+     */
     public void setOnCargoClickListener(OnCargoGoodClickListener listener) {
         this.listener = listener;
     }
