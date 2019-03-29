59edc1d977f08f4a99d84ecb7cd56f728f40af47
Alex Gurung <aag1234@gmail.com>
2019-03-28 23:46:00 -0400

    Firebase shows high score

--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/MainActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.spacetrader.View;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.spacetrader.Entity.Location;
 import com.example.spacetrader.R;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static android.support.constraint.Constraints.TAG;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
     private Button loadGameButton;
+    private Button highScoreButton;
+    private String scoreString = "";
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpFirebase();
 
         /* Button that moves from MainActivity to ConfigurationActivity*/
         startButton = findViewById(R.id.start_button);
         loadGameButton = findViewById(R.id.load_game_button);
+        highScoreButton = findViewById(R.id.high_score_button);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +51,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        highScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setTitle("Global High Scores");
+                alertDialogBuilder.setMessage(scoreString);
+                alertDialogBuilder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+    }
+    public void setUpFirebase() {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("scores");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                updateScore(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    public void updateScore(String value) {
+        scoreString = value;
+        if (scoreString != null) {
+            String[] scores_list = scoreString.split(", ");
+            ArrayList<Double> scores_doubles = new ArrayList<>();
+            for (String s : scores_list) {
+                scores_doubles.add(Double.parseDouble(s));
+            }
+            Collections.sort(scores_doubles);
+            Collections.reverse(scores_doubles);
+            scoreString = "";
+            for (Double d : scores_doubles) {
+                scoreString += String.format("%.2f\n", d);
+            }
+        }
+        Log.d(TAG, "New High Scores: " + value);
     }
 }
