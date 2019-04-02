316fc02e71829c252483c6138eca467318a2736f
Aman Jain <aman.jain@gatech.edu>
2019-04-02 19:06:36 -0400

    made a unit test for meself

---
+++ SpaceTrader/app/src/test/java/com/example/spacetrader/AmanUnitTest.java
@@ -0,0 +1,26 @@
+package com.example.spacetrader;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AmanUnitTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
+
+
+
