6ae402422f734d63f9be440fec4b070fbdc1850b
Aman Jain <aman.jain@gatech.edu>
2019-04-02 19:29:24 -0400

    wrote the setPrice unit test

--- SpaceTrader/app/src/test/java/com/example/spacetrader/AmanUnitTest.java
+++ SpaceTrader/app/src/test/java/com/example/spacetrader/AmanUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.spacetrader;
 
+import com.example.spacetrader.Entity.*;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -10,11 +12,62 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class AmanUnitTest {
+    private GoodType[] goodTypes;
+    private TechLevel[] techLevels;
+    private Resource[] resources;
+    private TraderCapability traderCapability;
+    private double[] prices;
+    private static final double delta = 0.00001;
 
     @Before
-    public void setup() {
-
+    public void setupSetPrice() {
+        goodTypes = GoodType.values();
+        techLevels = TechLevel.values();
+        resources = Resource.values();
+        traderCapability = new Trader();
+        prices = new double[1120];
+        int i = 0;
+        for(GoodType gt : goodTypes) {
+            for(TechLevel tl : techLevels) {
+                for(Resource r : resources) {
+                    int min = gt.getMinPrice();
+                    int max = gt.getMaxPrice();
+                    boolean isNaturalGood = gt.getIsNaturalResource();
+                    int tlordinal = tl.ordinal();
+                    if(!isNaturalGood) {
+                        tlordinal = 8 - tlordinal;
+                    }
+                    double add = ((max - min) * (double)tlordinal)/8;
+                    double pr = min + add;
+                    if(r.equals(gt.getHighCostResource())) {
+                        pr *= 2;
+                    }
+                    if(r.equals(gt.getLowCostResource())) {
+                        pr *= 0.5;
+                    }
+                    prices[i++] = pr;
+                }
+            }
+        }
     }
+
+    @Test
+    public void checkAllPrices() {
+        double[] calculatedPrices = new double[1120];
+        int i = 0;
+        for(GoodType gt : goodTypes) {
+            for(TechLevel tl : techLevels) {
+                for(Resource r : resources) {
+                    Good test = new Good(gt);
+                    traderCapability.setPrice(test, tl, r);
+                    calculatedPrices[i++] = test.getPrice();
+                }
+            }
+        }
+        Assert.assertArrayEquals(calculatedPrices, prices, delta);
+    }
+
+
 
     @Test
     public void addition_isCorrect() {
