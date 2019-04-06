b1961cf19f34b59198c8552fc46c08cde4f4b58e
Theresa Hsieh <theresaying.hsieh@gmail.com>
2019-04-06 10:40:00 -0400

    deleted isIllegal from GoodType

--- SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/GoodType.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/Entity/GoodType.java
@@ -6,21 +6,20 @@ import java.io.Serializable;
  * an enum class that contains the attributes and methods necessary to make up each type of GoodType
  */
 public enum GoodType implements Serializable {
-    WATER(30, 54, true, false, 0, 0, Resource.LOTSOFWATER, Resource.DESERT),
-    FUR(250, 320, true, false, 0, 0, Resource.RICHFAUNA, Resource.LIFELESS),
-    FOOD(105, 135, true, false, 1, 0, Resource.RICHSOIL, Resource.POORSOIL),
-    ORE(390, 490, true, false, 2, 2, Resource.MINERALRICH, Resource.MINERALPOOR),
-    ENTERTAINMENT(180, 240, false, false, 3, 1, Resource.ARTISTIC, Resource.NEVER),
-    FIREARM(725, 1175, false, true, 3, 1, Resource.WARLIKE, Resource.NEVER),
-    MEDICINE(510, 630, false, false, 4, 1, Resource.LOSTSOFHERBS, Resource.NEVER),
-    MACHINE(690, 810, false, false, 4, 3, Resource.NEVER, Resource.NEVER),
-    NARCOTIC(2620, 3500, false, true, 5, 0, Resource.WIERDMUSHROOMS, Resource.NEVER),
-    ROBOT(3950, 4400, false, false, 6, 4, Resource.NEVER, Resource.NEVER);
+    WATER(30, 54, true, 0, 0, Resource.LOTSOFWATER, Resource.DESERT),
+    FUR(250, 320, true, 0, 0, Resource.RICHFAUNA, Resource.LIFELESS),
+    FOOD(105, 135, true, 1, 0, Resource.RICHSOIL, Resource.POORSOIL),
+    ORE(390, 490, true, 2, 2, Resource.MINERALRICH, Resource.MINERALPOOR),
+    ENTERTAINMENT(180, 240, false, 3, 1, Resource.ARTISTIC, Resource.NEVER),
+    FIREARM(725, 1175, false, 3, 1, Resource.WARLIKE, Resource.NEVER),
+    MEDICINE(510, 630, false, 4, 1, Resource.LOSTSOFHERBS, Resource.NEVER),
+    MACHINE(690, 810, false, 4, 3, Resource.NEVER, Resource.NEVER),
+    NARCOTIC(2620, 3500, false, 5, 0, Resource.WIERDMUSHROOMS, Resource.NEVER),
+    ROBOT(3950, 4400, false, 6, 4, Resource.NEVER, Resource.NEVER);
 
     private final int minPrice;
     private final int maxPrice;
     private final boolean isNaturalResource;
-    private final boolean isIllegal;
     private final int minTechLevelProduce;
     private final int minTechLevelUse;
     private final Resource lowCostResource;
@@ -35,8 +34,6 @@ public enum GoodType implements Serializable {
      *                 moment in the universe
      * @param isNaturalResource of type boolean that represents whether said resource is a natural
      *                          resource
-     * @param isIllegal of type boolean that represents whether said resource is an illegal
-     *                  resource
      * @param lowCostResource of type Resource that represents the cost of said resource based on
      *                        positively affecting environmental conditions of the solar
      *                        systems/planet
@@ -45,12 +42,11 @@ public enum GoodType implements Serializable {
      *                         systems/planet
      */
     GoodType(int minPrice, int maxPrice, boolean isNaturalResource,
-             boolean isIllegal, int minTechLevelProduce, int minTechLevelUse,
+             int minTechLevelProduce, int minTechLevelUse,
              Resource lowCostResource, Resource highCostResource) {
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
         this.isNaturalResource = isNaturalResource;
-        this.isIllegal = isIllegal;
         this.minTechLevelProduce = minTechLevelProduce;
         this.minTechLevelUse = minTechLevelUse;
         this.lowCostResource = lowCostResource;
@@ -102,15 +98,6 @@ public enum GoodType implements Serializable {
     public boolean getIsNaturalResource() {
         return isNaturalResource;
     }
-
-//    /**
-//     * getter method for whether the current good type is illegal
-//     *
-//     * @return a boolean representation of whether the current good type is illegal
-//     */
-//    public boolean getIsIllegal() {
-//        return isIllegal;
-//    }
 
     /**
      * a getter method for the minimum tech level produce of the current good type
