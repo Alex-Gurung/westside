46f19af21446a0d5015d06bb871b19796e312379
Alex Gurung <aag1234@gmail.com>
2019-04-19 13:27:34 -0400

    fixed timeout

--- SpaceTrader/app/src/main/java/com/example/spacetrader/View/TravelActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/View/TravelActivity.java
@@ -1,6 +1,7 @@
 package com.example.spacetrader.View;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
@@ -36,13 +37,22 @@ public class TravelActivity extends AppCompatActivity {
         int diff = tradingViewModel.getGameDifficulty();
         switch (scenario) {
             case 0:
-                Toast.makeText(TravelActivity.this, "Traveled safely",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(TravelActivity.this, "Traveled safely", Toast.LENGTH_SHORT).show();
+                handler.postDelayed(() -> {
+                    // Actions to do after 2 seconds
+                    Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
+                    startActivityForResult(intent, 2);
+                }, 3000);
                 break;
             case 1:
                 Toast.makeText(TravelActivity.this, "You lost 100 credits",
                         Toast.LENGTH_SHORT).show();
                 tradingViewModel.setPlayerCredits(tradingViewModel.getPlayerCredits() - 100);
+                handler.postDelayed(() -> {
+                    // Actions to do after 2 seconds
+                    Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
+                    startActivityForResult(intent, 2);
+                }, 3000);
                 break;
             case 2:
                 double gainCredits = (600 - (100 * diff));
@@ -51,6 +61,11 @@ public class TravelActivity extends AppCompatActivity {
                 tradingViewModel.setPlayerCredits(
                         tradingViewModel.getPlayerCredits() + (600 - (100 * diff))
                 );
+                handler.postDelayed(() -> {
+                    // Actions to do after 2 seconds
+                    Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
+                    startActivityForResult(intent, 2);
+                }, 3000);
                 break;
             case 3:
                 Toast.makeText(TravelActivity.this, "A trader appears!", Toast.LENGTH_SHORT).show();
@@ -59,21 +74,22 @@ public class TravelActivity extends AppCompatActivity {
                 );
                 alertDialogBuilder.setTitle("Do you want to trade?");
                 alertDialogBuilder.setPositiveButton("Trade", (dialog, which) -> {
-                    
                     Intent intent = new Intent(getApplicationContext(), TraderActivity.class);
-                    startActivity(intent);
-
+                    startActivityForResult(intent, 1);
                 });
                 alertDialogBuilder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+                alertDialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
+                        startActivityForResult(intent, 2);
+                    }
+                });
                 AlertDialog alertDialog = alertDialogBuilder.create();
                 alertDialog.show();
                 break;
         }
-        handler.postDelayed(() -> {
-            // Actions to do after 2 seconds
-            Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
-            startActivityForResult(intent, 2);
-        }, 3000);
+
     }
 
     /**
@@ -85,7 +101,12 @@ public class TravelActivity extends AppCompatActivity {
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        finish();
+        if (requestCode == 2) {
+            finish();
+        } else if (requestCode == 1) {
+            Intent intent = new Intent( getApplicationContext(), SpacePortActivity.class);
+            startActivityForResult(intent, 2);
+        }
     }
 
 
